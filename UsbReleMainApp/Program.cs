676fb5abc051a631c4bf58b77aa58c56903fdcbf
Oleg Mursalov <olofovich@mail.ru>
2021-04-11 01:39:05 +0300

    ADDED NEW

--- UsbReleMainApp/Program.cs
+++ UsbReleMainApp/Program.cs
@@ -1,9 +1,11 @@
 ﻿using NLog;
 using System;
 using System.Collections.Generic;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UsbReleMainApp.Structure;
 
 namespace UsbReleMainApp
 {
@@ -14,6 +16,29 @@ namespace UsbReleMainApp
         public static void Main()
         {
             _logger.Info("Запуск консольки");
+
+            SerialPort port = new SerialPort("COM4", 9600, Parity.None, 8, StopBits.One);
+            try
+            {
+                port.Open();
+                port.Write("RELx.ON"); /* You can pass any command as per your documentation in port.Write */
+                System.Threading.Thread.Sleep(3000);
+                port.Write("RELx.OFF");
+                Environment.Exit(0);
+            }
+            catch (Exception ex)
+            {
+                if (ex is System.IO.IOException)
+                {
+                    Console.WriteLine("Port Exception: " + ex.ToString());
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("General Exception: " + ex.ToString());
+                    Console.ReadLine();
+                }
+            }
         }
     }
 }
---
+++ UsbReleMainApp/Structure/UsbWorker.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Management;
+
+namespace UsbReleMainApp.Structure
+{
+    public class USBDeviceInfo
+    {
+        public USBDeviceInfo(string deviceID, string pnpDeviceID, string description)
+        {
+            this.DeviceID = deviceID;
+            this.PnpDeviceID = pnpDeviceID;
+            this.Description = description;
+        }
+        public string DeviceID { get; private set; }
+        public string PnpDeviceID { get; private set; }
+        public string Description { get; private set; }
+    }
+
+    public class UsbWorker
+    {
+        public static List<USBDeviceInfo> GetUSBDevices()
+        {
+            List<USBDeviceInfo> devices = new List<USBDeviceInfo>();
+
+            ManagementObjectCollection collection;
+            using (var searcher = new ManagementObjectSearcher(@"Select * From Win32_USBHub"))
+                collection = searcher.Get();
+
+            foreach (var device in collection)
+            {
+                devices.Add(new USBDeviceInfo(
+                (string)device.GetPropertyValue("DeviceID"),
+                (string)device.GetPropertyValue("PNPDeviceID"),
+                (string)device.GetPropertyValue("Description")
+                ));
+            }
+
+            collection.Dispose();
+            return devices;
+        }
+    }
+}
